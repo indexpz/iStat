8fca52480066b2f9d6dc0ce3246c08682e85e3f1
Paweł Zieliński <pawelz72@gmail.com>
2021-06-10 11:45:25 +0200

    initial commit

---
+++ src/main/java/pl/indexpz/iStat/IStatApplication.java
@@ -0,0 +1,13 @@
+package pl.indexpz.iStat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IStatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IStatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/indexpz/iStat/IStatApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.indexpz.iStat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IStatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
