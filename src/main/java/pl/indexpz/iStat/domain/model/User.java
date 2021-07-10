8a6aa395b3f46f30f3919788431415991d8024e1
Paweł Zieliński <pawelz72@gmail.com>
2021-07-10 08:46:01 +0200

    Continue validation

--- src/main/java/pl/indexpz/iStat/domain/model/User.java
+++ src/main/java/pl/indexpz/iStat/domain/model/User.java
@@ -24,12 +24,17 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false, unique = true)
+    @NotBlank
+    @Email
     private String username;
     @Column(name = "first_name")
+    @NotBlank
     private String firstName;
     @Column(name = "last_name")
+    @NotBlank
     private String lastName;
     @Column(nullable = false)
+    @NotBlank
     private String password;
     @Column(nullable = false)
     private String role;
